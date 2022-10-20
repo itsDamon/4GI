9bdf28d2f1511f038ba12fd6129f9ba913afc9a8
itsDamon <nick.parise20@gmail.com>
2022-10-20 11:57:16 +0200

    orario

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 let data = new Date();
 let ora = data.getHours();
+if (ora < 10) ora = "0"+ ora
 let min = data.getMinutes();
 if (min < 10) min = "0" + min;
 let msg = "Sono le " + ora + ":" + min;
