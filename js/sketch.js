0be73e095dd9f3a3b74fe68af2572e911c3679af
itsDamon <nick.parise20@gmail.com>
2022-10-20 11:55:48 +0200

    aggiusto tutto

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,6 @@
 let data = new Date();
 let ora = data.getHours();
 let min = data.getMinutes();
-if (min < 10)
-    min = "0" + min;
+if (min < 10) min = "0" + min;
 let msg = "Sono le " + ora + ":" + min;
 document.getElementById("pOrario").innerHTML = msg;
