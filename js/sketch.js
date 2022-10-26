e44a1e83c57c5e2eaf9c1b1e9a57cb085e6f649b
itsDamon <nick.parise20@gmail.com>
2022-10-26 14:55:29 +0200

    adding aside dat

--- js/sketch.js
+++ js/sketch.js
@@ -5,3 +5,23 @@ let min = data.getMinutes();
 if (min < 10) min = "0" + min;
 let msg = "Sono le " + ora + ":" + min;
 document.getElementById("pOrario").innerHTML = msg;
+
+
+
+let imgBuongiono = document.getElementById("imgBuongiorno");
+let pBuongiorno = document.getElementById("pBuongiorno");
+
+let giorno = data.getDay();
+imgBuongiorno.src = `../img/giorno${giorno}`;
+
+let days = {
+    0: "domenica",
+    1: "lunedì",
+    2: "martedì",
+    3: "mercoledì",
+    4: "giovedì",
+    5: "venerdì",
+    6: "sabato"
+}
+
+pBuongiorno.innerHTML = `Buon ${days[giorno]}`;
